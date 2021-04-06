4d3072718936f324a1cc60a2c87d330d0c4eaf58
Jauhar Wibisono <jauhar.wibisono@gmail.com>
2021-04-06 10:52:56 +0700

    sukses nggambar garis pertama

--- src/interactiveMap.py
+++ src/interactiveMap.py
@@ -6,22 +6,36 @@ app = Flask(__name__)
 UPLOAD_FOLDER = os.path.abspath('../test') # directory dokumen-dokumen
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def readInput(filename):
+    # format input:
+    # Baris pertama berisi bilangan bulat N yang menyatakan banyak simpul
+    # N baris berikutnya berisi sebuah string dan dua bilangan bulat yang menyatakan nama dan koordinat simpul
+    # N baris berikutnya berisi adjacency matrix (sementara boolean)
+    inp = open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    N = int(inp.readline())
+    nodes = []
+    edge_list = []
+    for i in range(N):
+        node_name, lng, lat = inp.readline().split(' ')
+        nodes.append(node_name, float(lng), float(lat))
+    for i in range(N):
+        row = f.readline().split(' ')
+        for j in range(i):
+            if row[j] == 1:
+                edge_list.append((nodes[i][1], nodes[i][2], nodes[j][1], nodes[j][2]))
+    return (N, nodes, edge_list)
+
 # main page
 @app.route('/', methods=['GET', 'POST'])
 def MainPage():
     if request.method == 'POST':
         if 'upload' in request.files: # website dapet input file
             # dapatkan dan simpan file input
-            input_file = request.files['upload']
-            input_filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], input_filename))
+            ## input_file = request.files['upload']
+            ## input_filename = secure_filename(input_file.filename)
+            ## file.save(os.path.join(app.config['UPLOAD_FOLDER'], input_filename))
             # baca input
-            # format input:
-            # Baris pertama berisi bilangan bulat N yang menyatakan banyak simpul
-            # N baris berikutnya berisi sebuah string dan dua bilangan bulat yang menyatakan nama dan koordinat simpul
-            # N baris berikutnya berisi adjacency matrix
-            # inp = open(os.path.join(app.config['UPLOAD_FOLDER'], input_filename))
-            # for line in inp
+            ## N, nodes, edge_list = readInput(input_filename)
             # jalankan algoritme A*
             distance = 100 # testing value
             path = [('Bucharest', 44.457, 26.093), ('Arad', 46.181, 21.312), ('Zerind', 46.624, 21.518)] # testing value
